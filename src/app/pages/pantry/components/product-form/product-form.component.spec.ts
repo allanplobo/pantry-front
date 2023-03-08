ccf681773630d70f6457c1facd74e03806083eb7
Allan Lobo <allan.patrick@grupobrisanet.com.br>
2023-03-07 23:41:29 -0300

    tests: create form validations tests to product form

--- src/app/pages/pantry/components/product-form/product-form.component.spec.ts
+++ src/app/pages/pantry/components/product-form/product-form.component.spec.ts
@@ -1,23 +1,79 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
+import { IonicModule, ModalController } from '@ionic/angular';
 
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ProductsService } from './../../services/products.service';
 import { ProductFormComponent } from './product-form.component';
 
 describe('ProductFormComponent', () => {
   let component: ProductFormComponent;
   let fixture: ComponentFixture<ProductFormComponent>;
+  let productsServiceSpy: jasmine.SpyObj<ProductsService>;
+  let modalControllerSpy: jasmine.SpyObj<ModalController>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [IonicModule.forRoot(), ProductFormComponent],
+      imports: [
+        IonicModule.forRoot(),
+        ReactiveFormsModule,
+        ProductFormComponent,
+        HttpClientTestingModule,
+      ],
+      providers: [
+        { provide: ProductsService, useValue: productsServiceSpy },
+        { provide: ModalController, useValue: modalControllerSpy },
+      ],
     }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(ProductFormComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('form validation', () => {
+    it('should be invalid when name is not provided', () => {
+      const form = component.productForm;
+      form.patchValue({ description: 'test', quantity: 1, price: 2 });
+      expect(form.valid).toBeFalsy();
+      expect(form.get('name')?.errors?.['required']).toBeTruthy();
+    });
+
+    it('should be invalid when name is too short', () => {
+      const form = component.productForm;
+      form.patchValue({
+        name: 'a',
+        description: 'test',
+        quantity: 1,
+        price: 2,
+      });
+      expect(form.valid).toBeFalsy();
+      expect(form.get('name')?.errors?.['minlength']).toBeTruthy();
+    });
+
+    it('should be invalid when name is too long', () => {
+      const form = component.productForm;
+      const name = 'a'.repeat(101);
+      form.patchValue({ name, description: 'test', quantity: 1, price: 2 });
+      expect(form.valid).toBeFalsy();
+      expect(form.get('name')?.errors?.['maxlength']).toBeTruthy();
+    });
+
+    it('should be valid when all fields are provided', () => {
+      const form = component.productForm;
+      form.patchValue({
+        name: 'test',
+        description: 'test',
+        quantity: 1,
+        price: 2,
+      });
+      expect(form.valid).toBeTruthy();
+    });
   });
 });
