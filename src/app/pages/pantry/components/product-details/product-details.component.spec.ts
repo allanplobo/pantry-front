1b9eb248f897143d2c77645a4a5232108c9373ed
Allan Lobo <allan.patrick@grupobrisanet.com.br>
2023-03-09 20:05:53 -0300

    tests: write unit tests to form and validations in product details component

--- src/app/pages/pantry/components/product-details/product-details.component.spec.ts
+++ src/app/pages/pantry/components/product-details/product-details.component.spec.ts
@@ -3,10 +3,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { IonicModule, ModalController } from '@ionic/angular';
 
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { IProduct } from '../../models/product';
 import { ProductsService } from '../../services/products.service';
 import { ProductDetailsComponent } from './product-details.component';
 
 // TODO: write unit tests
+
+const productMock: IProduct = {
+  _id: '1',
+  name: 'test',
+  description: 'description',
+  quantity: 1,
+  price: 1,
+};
 
 describe('ProductDetailsComponent', () => {
   let component: ProductDetailsComponent;
@@ -39,7 +48,38 @@ describe('ProductDetailsComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('form validation', () => {
+  describe('forms and validators', () => {
+    it('should not populate form on init when component have productInfo', () => {
+      const populateFormSpy = spyOn(component, 'populateForm');
+      component.ngOnInit();
+      expect(populateFormSpy).not.toHaveBeenCalled();
+    });
+
+    it('should populate form on init when component have productInfo', () => {
+      component.productInfo = productMock;
+      component.ngOnInit();
+      expect(component.productForm.get('name')?.value).toEqual(
+        productMock.name
+      );
+      expect(component.productForm.get('description')?.value).toEqual(
+        productMock.description
+      );
+      expect(component.productForm.get('quantity')?.value).toEqual(
+        productMock.quantity
+      );
+      expect(component.productForm.get('price')?.value).toEqual(
+        productMock.price
+      );
+      expect(component.productForm.disabled).toBeTrue();
+    });
+
+    it('should not disable form if enableProductForm is true', () => {
+      component.productInfo = productMock;
+      component.enableProductForm = true;
+      component.ngOnInit();
+      expect(component.productForm.disabled).toBeFalse();
+    });
+
     it('should be invalid when name is not provided', () => {
       const form = component.productForm;
       form.patchValue({ description: 'test', quantity: 1, price: 2 });
