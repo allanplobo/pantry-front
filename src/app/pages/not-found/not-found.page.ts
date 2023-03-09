3694a65d5ef80b799f415f61b99e59a97fce1c31
Allan Lobo <allan.patrick@grupobrisanet.com.br>
2023-03-08 21:47:36 -0300

    feat: create the UI and routing in not found page

--- src/app/pages/not-found/not-found.page.ts
+++ src/app/pages/not-found/not-found.page.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-not-found',
   templateUrl: './not-found.page.html',
   styleUrls: ['./not-found.page.scss'],
 })
-export class NotFoundPage implements OnInit {
+export class NotFoundPage {
+  constructor(private navController: NavController) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  backToPantryPage() {
+    this.navController.navigateRoot('/tabs/pantry');
   }
-
 }
